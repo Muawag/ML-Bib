a025302d0b65b00774579a3d2c2b8e17039664c0
Muawag <101280259+Muawag@users.noreply.github.com>
2026-09-15 13:27:49 +0200

    LinRegression1 Update

---
+++ LinearRegression.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Regressor.h"
+
+
+class LinearRegression : public Regressor {
+    public:
+
+    private:
+        std::vector<double> params;
+};
---
+++ Regressor.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Matrix.h" 
+#include <vector>
+
+
+class Regressor {
+    public:
+        virtual void train(const Matrix& x_daten, const Matrix& y_daten) = 0;
+        virtual std::vector<double> predict(const Matrix& predict_daten) const = 0;
+        
+        private:
+};
